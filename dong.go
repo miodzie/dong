4d513c1dcf0ebe46d7f7166529ebb65ec6ce57a7
miodzie <miodzie@protonmail.com>
2022-09-16 23:05:01 -0400

    No need for a separate file.

--- dong.go
+++ dong.go
@@ -5,6 +5,14 @@ type Emoji struct {
 	Category string
 }
 
+// Fetcher fetches dongs in any fashion desired.
+// There is a default Web Scraper implementation of this from
+// http://dongerlist.com in the commandline package.
+// It returns the dongs found and any error encountered.
+type Fetcher interface {
+	Fetch() ([]Emoji, error)
+}
+
 type Repository interface {
 	Random() (Emoji, error)
 	RandomByCategory(string) (Emoji, error)
--- importer.go
+++
@@ -1,9 +0,0 @@
-package dong
-
-// Fetcher fetches dongs in any fashion desired.
-// There is a default Web Scraper implementation of this from
-// http://dongerlist.com in the commandline package.
-// It returns the dongs found and any error encountered.
-type Fetcher interface {
-	Fetch() ([]Emoji, error)
-}
